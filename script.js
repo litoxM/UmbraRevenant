3c497685e2180a5072176683f1e131d6131bfdbe
litoxM <pcarl5136@gmail.com>
2025-02-14 22:51:49 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("enter-btn").addEventListener("click", function() {
+    this.innerText = "Loading...";
+    this.disabled = true;
+
+    // Start music
+    let music = document.getElementById("bg-music");
+    music.play().catch(error => console.log("Autoplay blocked"));
+
+    setTimeout(() => {
+        window.location.href = "main.html"; // Redirect to the main page (to be created)
+    }, 2000);
+});
